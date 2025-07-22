eea7297fd9220dda1721a77a1b87bf4d9fa7c112
kimwoojin <woojin200232@gmail.com>
2025-07-22 14:55:31 +0900

    vector-pipeline-error-revised

--- test-ML-backend/training_HSI/train_vector.py
+++ test-ML-backend/training_HSI/train_vector.py
@@ -68,7 +68,7 @@ def main():
     csv_path = config.get('data', {}).get('csv', 'data/vector_data.csv')
     column_config_path = config.get('data', {}).get(
         'column_config', 'configs/HSI_vector/column_config.json')
-    scaler = config.get('scaler', StandardScaler)
+    scaler = config.get('scaler', "standardscaler")
 
     setup_seed(seed)
 
@@ -128,7 +128,7 @@ def main():
 
         # 훈련 수행
         print("Start training...")
-        training_results, scaler = trainer.train(
+        training_results = trainer.train(
             X_train=X_train,
             y_train=y_train,
             K_fold=K_fold,
