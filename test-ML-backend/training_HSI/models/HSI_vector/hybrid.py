5d6e6e5ddd77d06f4ea2e398d6f04e12946fb3b3
vineyard <bear5284@korea.ac.kr>
2025-07-24 11:51:17 +0900

    mod

--- test-ML-backend/training_HSI/models/HSI_vector/hybrid.py
+++ test-ML-backend/training_HSI/models/HSI_vector/hybrid.py
@@ -1,18 +1,20 @@
-import xgboost as xgb
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
-import seaborn as sns
-import pandas as pd
-import numpy as np
 import os
-from torchvision import transforms
-from typing import Dict
+import numpy as np
+import pandas as pd
 import cv2
-from sklearn.decomposition import PCA
+from typing import Dict, List
 from tqdm import tqdm
-import torch
-from transformers import ViTModel, ViTFeatureExtractor
 from joblib import Parallel, delayed
 import multiprocessing
+
+import torch
+from torchvision import transforms
+from transformers import ViTMoel, ViTImageProcessor
+import xgboost as xgb
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.decomposition import PCA
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 class HybridModel:
@@ -29,6 +31,10 @@ class HybridModel:
             transforms.Resize((224, 224)),
             transforms.ToTensor()
         ])
+
+        # Feature Extractor Backbone
+        self.processor = ViTImageProcessor.from_pretrained('google/vit-base-patch16-224-in21k')
+        self.processor.eval()
 
         self.TS_path = config.get("TS_path", None)
         self.VS_path = config.get("VS_path", None)
@@ -49,6 +55,8 @@ class HybridModel:
             eval_metric='logloss',
             n_jobs=-1
         )
+        self.model = MultiOutputClassifier(self.model)
+
     # 하나의 폴더 내에 _P로 시작하는 이미지들 path 불러온 뒤 여러 파장대들 묶어 하나의 tensor로 반환
     def load_stack(self, id_folder):
         band_files = sorted(os.listdir(id_folder), key=lambda x: int(x.split('_P')[-1].split('.')[0]))
@@ -65,12 +73,11 @@ class HybridModel:
         flat = stack_img.reshape(-1, C)
 
         pca = PCA(n_components=3)
-        reduced = pca.fit_transform(flat)
-        reduced_img = reduced.reshape(H, W, 3)
-        reduced_img -= reduced_img.min()
-        reduced_img /= reduced_img.max()
-        reduced_img *= 255
-        return reduced_img.astype(np.uint8)
+        reduced = pca.fit_transform(flat).reshape(H, W, 3)
+
+        # 0 ~ 255 정규화
+        reduced = (reduced - reduced.min()) / (reduced.max() - reduced.min() + 1e-6)
+        return (reduced * 255).astype(np.nint8)
 
     # 병렬화를 위한 작업 단일화
     def process_single_folder(self, folder_path):
@@ -104,15 +111,13 @@ class HybridModel:
         label_cols = [col for col in df.columns if col.startswith('disease_')]
         for label in label_cols:
             df_list.append(df[label].values)
-        return df_list
+        return np.array(df_list)
 
     def fit(self, X, y):
         train_list = self.process_all(self.TS_path)
         val_list = self.process_all(self.VS_path)
-        vit_model = ViTModel.from_pretrained('google/vit-base-patch16-224-in21k').to(DEVICE)
-        vit_model.eval()
-        vit_features_train = self.extract_features(vit_model, train_list)
-        vit_features_val = self.extract_features(vit_model, val_list)
+        vit_features_train = self.extract_features(self.processor, train_list)
+        vit_features_val = self.extract_features(self.processor, val_list)
 
         X_train, X_test = vit_features_train, vit_features_val
         y_train = np.array(self.load_labels(self.TS_label))
