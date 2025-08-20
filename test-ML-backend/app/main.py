eeb72fba34dd5f4c18870344f327c9255ea909b3
DDD <dindanehi@gmail.com>
2025-08-20 17:48:56 +0900

    main.py

--- test-ML-backend/app/main.py
+++ test-ML-backend/app/main.py
@@ -71,7 +71,7 @@ app.include_router(predict.router, prefix="/predict", tags=["prediction"])
 app.include_router(meat.router, prefix="/meat", tags=["meat"])  # 육류 데이터 관리
 app.include_router(user.router, prefix="/user", tags=["user"])  # 사용자 관리
 app.include_router(statistic_api.router, prefix="/statistic", tags=["statistic"])  # 통계 데이터 관리
-app.include_router(data.router, prefix="/data", tags=["data"])  # 데이터 관련 API
+app.include_router(data.router, prefix="/data-upload", tags=["data-upload"])  # 데이터 업로드 API
 app.include_router(data_crud.router, prefix="/data", tags=["data-crud"])  # 데이터 수정/삭제 API
 app.include_router(xai.router, prefix="/xai", tags=["explainable AI"])  # XAI 관련 API
 app.include_router(hsi_predict.router, prefix="/hsipredict", tags=["HSI prediction"])  # HSI 예측 API
