defef17db143242edb6691e6a133320306154182
vineyard <bear5284@korea.ac.kr>
2025-08-20 11:04:40 +0900

    firebase

--- test-ML-backend/app/core/firebase.py
+++ test-ML-backend/app/core/firebase.py
@@ -1,11 +1,10 @@
-# app/firebase_auth.py
-import os, firebase_admin
+import os, json, firebase_admin
 from firebase_admin import credentials
 
 def init_firebase():
     if not firebase_admin._apps:
         cred_path = os.getenv("FIREBASE_CREDENTIALS", "serviceAccountKey.json")
+        with open(cred_path) as f:
+            print("[Firebase] project_id =", json.load(f)["project_id"])
         cred = credentials.Certificate(cred_path)
         firebase_admin.initialize_app(cred)
-
-init_firebase()  # import 시 1회 초기화
