d3f3aaf659cb896823d28d4e44bf69f59ce185f8
DDD <dindanehi@gmail.com>
2025-08-20 16:22:15 +0900

    data.py

--- test-ML-backend/app/routers/data.py
+++ test-ML-backend/app/routers/data.py
@@ -22,7 +22,7 @@ s3_client = boto3.client(
     's3',
     aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
     aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY,
-    region_name=settings.AWS_REGION,
+    region_name=settings.S3_REGION_NAME,
     config=Config(signature_version='s3v4')
 )
 
