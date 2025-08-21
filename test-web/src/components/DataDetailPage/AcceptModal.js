45f2c0eea7a47993a1297689b316236639b7df5b
myeonghah <myeongha159357@gmail.com>
2025-08-21 15:15:41 +0900

    modify endpoint

--- test-web/src/components/DataDetailPage/AcceptModal.js
+++ test-web/src/components/DataDetailPage/AcceptModal.js
@@ -3,9 +3,8 @@ import { useNavigate, useLocation } from 'react-router-dom';
 import { Backdrop, Box, Modal, Fade, Button, Typography } from '@mui/material';
 import { FaRegCheckCircle } from 'react-icons/fa';
 import StateChangedModal from './StateChangedModal';
-import updateDataStatus from '../../API/updateDataStatus';
+import { confirmMeat } from '../../API/updateDataStatus';
 import style from './style/acceptmodalstyle';
-
 
 const navy = '#0F3659';
 
@@ -23,7 +22,7 @@ export const AcceptModal = ({ meatId, setConfirmVal, confirmVal }) => {
 
   // 승인 Api 호출
   const changeConfirmState = () => {
-    updateDataStatus(confirmVal, meatId, setStateChanged);
+    confirmMeat(meatId, setStateChanged);
   };
   // 최종 변경 완료 팝업
   const [stateChanged, setStateChanged] = useState(false);
@@ -138,4 +137,3 @@ export const AcceptModal = ({ meatId, setConfirmVal, confirmVal }) => {
   );
 };
 export default AcceptModal;
-
